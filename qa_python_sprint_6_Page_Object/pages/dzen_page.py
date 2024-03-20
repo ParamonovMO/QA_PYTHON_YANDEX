9bb077ae7f11a6c2bdd6d837ea1fafaafcc26324
ParamonovMO <paramonov.maxim@vk.com>
2024-03-20 17:39:13 +0500

    add sprint_6

---
+++ qa_python_sprint_6_Page_Object/pages/dzen_page.py
@@ -0,0 +1,10 @@
+from pages.base_page import BasePage
+from locators.dzen_page_locators import DzenPageLocators
+import allure
+
+
+class DzenPage(BasePage):
+
+    @allure.step('Проверка отображения кнопки "Главная" на странице DZEN')
+    def check_element_main_button(self):
+        return self.check_element(DzenPageLocators.main_button_dzen)
